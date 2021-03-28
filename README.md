6f6a31de830da372996de4db571c3ea24b0fbaf5
David MacCallum <davidjohnmac@gmail.com>
2021-03-28 17:25:06 +0200

    Working

---
+++ README.md
@@ -0,0 +1,26 @@
+# Send SMS
+
+Sends an SMS using Twilio.
+
+## Getting Started
+
+Create a .env file with the following environment variables.
+
+```
+TWILIO_ACCOUNT_SID='xxx'
+TWILIO_AUTH_TOKEN='xxx'
+FROM_NUMBER='xxx'
+```
+
+Start the server.
+
+```sh
+npm install
+npm start
+```
+
+Send a message (replace to param with your number).
+
+```sh
+curl http://localhost:9000/send-welcome-sms -d '{"name":"Paul", "to":"+27111111111"}' --header "Content-Type: application/json"
+```
--- app.js
+++ app.js
@@ -1,7 +1,62 @@
+require('dotenv').config()
+var bodyParser = require('body-parser')
+const Twilio = require('twilio');
 const express = require('express')
+const coms = require('./lib/coms')
+
 const app = express()
+app.use(bodyParser.json())
 
+var accountSid = process.env.TWILIO_ACCOUNT_SID;
+var token = process.env.TWILIO_AUTH_TOKEN;
+var from = process.env.FROM_NUMBER;
 
+var twilio = new Twilio(accountSid, token);
+
+app.post('/send-welcome-sms', (req, res) => {
+
+  if (!req.body.name || !req.body.to) {    
+    return res.status(400).send({
+      err: 'name and to required'
+    })
+  }
+
+  const welcomeMessage = coms.buildWelcomeSms({ 
+    name: req.body.name, 
+    to: req.body.to,
+    from
+  })
+  
+  console.debug("Sending message", welcomeMessage)
+
+  twilio.messages.create(welcomeMessage, (err, smsRes) => {
+    if (err) {
+      console.error('Error sending SMS.', welcomeMessage, err)
+      return res.status(500).send({
+        err: `Error sending SMS. ${err.message || err}`
+      })
+    }    
+    
+    console.debug("Sms sent.", smsRes)
+    res.send({
+      result: "Sent",
+      detail: welcomeMessage
+    })
+  })
+
+})
+
+app.use((req, res, _next) => {
+  res.status(404).send({
+    err: `No handler for ${req.method} ${req.path}`
+  })
+})
+
+app.use((err, _req, res, _next) => {
+  res.status(500).send({
+    err: err.message || err
+  })
+})
 
 app.listen(process.env.PORT || 9000, err => {
   if (err) return console.error("Could not start server", err)
